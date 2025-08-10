468f9edc04784e96d90f1063b8957a927e27044b
michaelzrork <michaelzrork@gmail.com>
2025-08-10 00:53:26 -0400

    Added hide on scroll for share button.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.example.rebrickable"
         minSdk = 24
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.0.1"
+        versionCode = 5
+        versionName = "1.0.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
